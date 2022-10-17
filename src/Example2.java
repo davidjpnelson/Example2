c1fee49bd98848b6cfd4e5a418b1e4fe0980983e
djpn <36060426+davidjpnelson@users.noreply.github.com>
2022-10-17 10:03:39 -0500

    cha cha cha changes

--- src/Example2.java
+++ src/Example2.java
@@ -3,6 +3,11 @@ public class Example2 {
   public static void main(String[] args) {
     int myVar1;
     int myVar2;
+    int x;
+    double v;
+
+    x = 10;
+    v = 10.0;
 
     myVar1 = 1024;
 
@@ -12,5 +17,15 @@ public class Example2 {
 
     System.out.print("myVar2 contains myVar1 / 2: ");
     System.out.println(myVar2);
+
+    System.out.println("Original value of x: " + x);
+    System.out.println("Original value of v: " + v);
+    System.out.println();
+
+    x = x / 4;
+    v = v / 4;
+
+    System.out.println("x after division: " + x);
+    System.out.println("v after division: " + v);
   }
 }
